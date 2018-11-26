1923c77c8662b21da5cb472958992b159a1f1515
Mateo Balcorta <misterrattail@gmail.com>
2018-11-26 15:04:28 -0800

    Remove business and veterans boxes.

--- src/containers/Form.jsx
+++ src/containers/Form.jsx
@@ -13,25 +13,22 @@ class Form extends Component {
         this.state.submitted = false
         this.state.countDown = 5
         this.state.isMobile = false
-        this.state.isBusiness = false
-        this.state.includeBusinessBox = false
         this.onSubmit = this.onSubmit.bind(this)
         this.closeModal = this.closeModal.bind(this)
-        this.business = this.business.bind(this)
     }
 
     componentDidMount(){
-      const swaps = [
-        '6CPDA','6CHV', '6CommonCause', '6CorporateAcct', '6Kos', '6DemandProgress','6DFA','6FOE', '6Greenpeace', '6OpenMedia', '6PeoplesAction', '6PFAW','6ProgressAmerica','6PCAF','6SumOfUs', '6Nation', '6Watchdog'
-      ]
+      // const swaps = [
+      //   '6CPDA','6CHV', '6CommonCause', '6CorporateAcct', '6Kos', '6DemandProgress','6DFA','6FOE', '6Greenpeace', '6OpenMedia', '6PeoplesAction', '6PFAW','6ProgressAmerica','6PCAF','6SumOfUs', '6Nation', '6Watchdog'
+      // ]
       
-      const includeBusinessBox = swaps.includes(this.state.source)
+      // const includeBusinessBox = swaps.includes(this.state.source)
 
-      if(includeBusinessBox){
-        this.setState({
-          includeBusinessBox: includeBusinessBox
-        })
-      }
+      // if(includeBusinessBox){
+      //   this.setState({
+      //     includeBusinessBox: includeBusinessBox
+      //   })
+      // }
     
       this.props.media({ minWidth: 768 }, () => {
         this.setState({
@@ -45,19 +42,10 @@ class Form extends Component {
       })
     }
 
-    business() {
-      const checkBoxStatus = document.getElementById("business").checked ? true : false;
-      this.setState({
-        isBusiness: checkBoxStatus
-      })
-    }
-
     render() {
       let modal = null;
       let topOfPage = null;
       let middle = null; 
-      let headerContent = null;
-      let businessForm = null; 
 
       const subHeader = (
         <div id="subHeader">          
@@ -75,31 +63,7 @@ class Form extends Component {
         <div className="flex" style={{marginBottom: '20px'}}>
           <input type="text" className="form-input" name="street" placeholder="Street Address" />
           <input type="text" className="form-input" name="zip" placeholder="Your Zipcode" />
-        </div>
-        <div style={{display: this.state.isBusiness ? 'block' : 'none'}}>
-          <div className="flex">
-            <input type="text" className="form-input" name="company_name" placeholder="Business Name*" />
-            <input type="text" className="form-input" name="company_website" placeholder="Business Website*" />
-          </div>
-          <div className="flex">
-            <input type="text" className="form-input" name="company_zipcode" placeholder="Business Zipcode*" />
-            <input type="text" className="form-input" name="company_phone" placeholder="Phone Number" />
-          </div>
-        </div>
-        <div style={{display: this.state.includeBusinessBox ? 'block' : 'none', marginBottom: '10px'}}>
-          <input id='veteran' name="veteran_checkbox" style={{padding:'0', margin:'0',verticalAlign:'bottom', position: 'relative', width: '20px', height: '20px', borderRadius: '5px', border: '2px solid #555'}} type="checkbox"/>
-            <label style={{display:'inline', textIndent:'15px'}}>
-              <strong>Check this box if you have served in the military and support net neutrality. Veterans and service members can play an important role in helping to save the open internet.
-              </strong>
-            </label>
-        </div>
-        <div style={{display: this.state.includeBusinessBox ? 'block' : 'none'}}>
-          <input id='business' onClick={ this.business } name="business_checkbox" style={{padding:'0', margin:'0',verticalAlign:'bottom', position: 'relative', width: '20px', height: '20px', borderRadius: '5px', border: '2px solid #555'}} type="checkbox"/>
-          <label style={{display:'inline', textIndent:'-15px'}}>
-            <strong>Check this box if you own or help run a small business and want to help save net neutrality. Small businesses will be among those hurt most by the loss of net neutrality, but they can also play a key role in saving it.
-            </strong>
-          </label>
-        </div>
+        </div>       
         <div className="flex" style={{marginTop: '25px'}}>
           <button className="btn">
             <span>{this.props.formButton}</span>
@@ -206,52 +170,6 @@ class Form extends Component {
             'source': this.state.source || 'website',
             'want_progress': 1,
         };
-
-        if(document.getElementById("veteran").checked){
-          fields['action_veteran'] = CONF.actionKitPageId
-        }
-
-        if(document.getElementById("business").checked){
-          const compName = form.company_name
-          if (!compName.value.trim()) {
-            compName.focus();
-            alert('Please enter your business name.');
-            return;
-          } else {
-            fields['action_company_name'] = compName.value.trim()
-          }
-
-          const compWebsite = form.company_website
-          if (!compWebsite.value.trim()) {
-            compWebsite.focus();
-            alert('Please enter your business website.');
-            return;
-          } else {
-            fields['action_company_website'] = compWebsite.value.trim()
-          }
-
-          const zip = form.company_zipcode;
-          if (!zip.value.trim()) {
-              zip.focus();
-              alert('Please enter your business zipcode.');
-              return;
-          } else if (zip.value.length < 5 || zip.value.length > 5) {
-              zip.focus();
-              alert('Please enter a valid business zipcode.');
-              return;
-          } else {
-            fields['action_company_zipcode'] = zip.value.trim()
-          }
-
-          const phone = form.company_phone
-          if(phone.value.trim() && phone.value.trim().length < 10 || /[a-z]/i.test(phone.value)|| phone.value.trim().length > 14){
-            phone.focus();
-            alert('Please enter a valid Phone number.');
-            return;
-          } else {
-            fields['action_company_phone'] = phone.value.trim()
-          }
-        }
       
         this.sendFormToActionKit(fields);
     }
@@ -264,17 +182,6 @@ class Form extends Component {
       firstRow[1].value = ''
       secondRow[0].value = ''
       secondRow[1].value = ''
-    }
-
-    clearBusinessForm(){
-      const formFlex = document.getElementById("form").getElementsByClassName("flex")
-      const thirdRow = formFlex[2].getElementsByClassName("form-input")
-      const fourthRow = formFlex[3].getElementsByClassName("form-input")
-      thirdRow[0].value = ''
-      thirdRow[1].value = ''
-      fourthRow[0].value = ''
-      fourthRow[1].value = ''
-      document.getElementById("business").checked = false
     }
 
     sendFormToActionKit(fields) {
@@ -303,16 +210,7 @@ class Form extends Component {
         form.submit()
         this.setState(
           { submitted: true }, 
-          () => {
-            this.clearUserForm()
-            if(document.getElementById("business").checked){
-              this.clearBusinessForm()
-            }
-
-            if(document.getElementById("veteran").checked){
-              document.getElementById("veteran").checked = false
-            }
-           });   
+          () => {this.clearUserForm()});   
     }
 
 }
