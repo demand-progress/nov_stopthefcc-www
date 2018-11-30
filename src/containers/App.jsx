db94df012f90674568ffb8b95589ec6ffac8baca
Mateo Balcorta <misterrattail@gmail.com>
2018-11-29 20:59:40 -0800

    Add visibility check.

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,14 +1,49 @@
 import React, { Component } from 'react';
 import Main from './Main.jsx'
 import Footer from './Footer.jsx'
+import Responsive from 'react-responsive-decorator';
 
-const App = () =>  { 
-    return(
-        <div>
-            <Main />
-            <Footer />
-        </div>
-    )
+
+class App extends Component  { 
+   constructor(props){
+       super(props)
+       this.state = {
+           showLogos: false,
+           isMobile: true
+       }
+       this.logoCheck = this.logoCheck.bind(this)
+   }
+
+   componentDidMount(){
+    this.props.media({ minWidth: 768 }, () => {
+        this.setState({
+          isMobile: false
+        }, () => {console.log('is mobile?', this.state.isMobile)})
+      })
+      this.props.media({ maxWidth: 768 }, () => {
+        this.setState({
+          isMobile: true
+        }, () => {console.log('is mobile?', this.state.isMobile)})
+      })
+   }
+
+   logoCheck(isVisible){
+    if(isVisible && this.state.isMobile){
+        this.setState({
+            showLogos: true
+        })
+      }
+   }
+   
+    render(){
+        return(
+            <div>
+                <Main logoCheck={this.logoCheck}/>
+                <Footer showLogos={this.state.showLogos} isMobile={this.state.isMobile}/>
+            </div>
+        )
+    }
+  
 }
 
-export default App;
+export default Responsive(App);
