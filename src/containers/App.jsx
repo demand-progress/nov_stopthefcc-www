5785ce9b6b2d10941fb0ce6f2167e1fa8f65985c
Xavier <munroexavier@gmail.com>
2019-05-01 19:23:44 -0400

    added the actionkit api data to the website.

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -9,9 +9,13 @@ class App extends Component  {
        super(props)
        this.state = {
            showLogos: false,
-           isMobile: true
+           isMobile: true,
+           statement_text: '',
+           about_text: '',
+           statement_leadin: '',
        }
        this.logoCheck = this.logoCheck.bind(this)
+       this.getActionKitData = this.getActionKitData.bind(this)
    }
 
    componentDidMount(){
@@ -25,6 +29,8 @@ class App extends Component  {
           isMobile: true
         }, () => {console.log('is mobile?', this.state.isMobile)})
       })
+
+      this.getActionKitData()
    }
 
    logoCheck(isVisible){
@@ -34,11 +40,31 @@ class App extends Component  {
         })
       }
    }
+
+   getActionKitData(){
+     fetch('https://cors-anywhere.herokuapp.com/https://vexq0wyt42.execute-api.us-east-1.amazonaws.com/default/dp-ak-service?page=2662')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data.objects[0])
+        this.setState({
+          statement_text: data.objects[0].statement_text,
+          about_text: data.objects[0].about_text,
+          statement_leadin: data.objects[0].statement_leadin,
+        })
+      })
+      .catch(err => console.log('FAILED TO FETCH AK API', err));
+   }
    
     render(){
         return(
             <div>
-                <Main logoCheck={this.logoCheck} isMobile={this.state.isMobile}/>
+                <Main 
+                  logoCheck={this.logoCheck} 
+                  isMobile={this.state.isMobile}
+                  statementText={this.state.statement_text}
+                  aboutText={this.state.about_text}
+                  statementLeadin={this.state.statement_leadin}
+                  />
                 <Footer showLogos={this.state.showLogos} isMobile={this.state.isMobile}/>
             </div>
         )
