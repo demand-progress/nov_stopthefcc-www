02e7930e7668daa0c93344f6fcc5bab7dee81bbe
Devin Dixon <ddixon@bingewave.com>
2019-06-26 22:48:53 -0400

    Trying new path route

--- src/app.jsx
+++ src/app.jsx
@@ -21,7 +21,7 @@ import Signup from './containers/Signup.jsx'
 })()
 
 const routing = (
-  <HashRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter path={process.env.PUBLIC_URL + '/'} >
     <div>
       <Route exact path="/" component={App} />
       <Route exact path="/signup" component={Signup} />
